30d9dc41400a748e79a3501f344444d3b55372a5
Konstantin Ru <kosru86@gmail.com>
2022-09-08 20:30:50 +0300

    added second diagram and second programm

---
+++ second/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам.
+//Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+//При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+Console.Clear();
+string[] first;
+string[] second;
+Init(out first);
+Solve(first, out second);
+Print();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void Init(out string[] arr)
+{
+    Console.Write("Введите количество строк в массиве:  ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    arr = new string[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write($"Введите строку № {(i + 1)}:  ");
+        arr[i] = Console.ReadLine();
+    }
+
+}
+
+void Solve(string[] arr, out string[] arrs)
+{
+    arrs = new string[first.Length];
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length < 4)
+        {
+            arrs[count] = arr[i];
+            count++;
+        }
+    }
+}
+
+void Print()
+{
+    System.Console.WriteLine();
+    System.Console.WriteLine("Итоговый массив: ");
+    for (int i = 0; i < second.Length; i++)
+        Console.WriteLine($"{second[i]} ");
+}
